35bdc5951f27bec06d64c4605aa7785ee819aa68
Qirui Sun <qiruisun@gmail.com>
2016-01-13 17:25:02 -0500

    Using each method to do the sum of the students number

--- exercise7.rb
+++ exercise7.rb
@@ -23,3 +23,8 @@ puts students
 # end  # need to find a way to convert the values in this hash to an new array!
 total_students = students.values.inject { |sum, n| sum + n }  #=>99  |sum| is the accumulator here which equals to the previous sum+n!!!
 puts total_students
+
+students.values.each do |value|
+  sum += value
+end
+puts sum
