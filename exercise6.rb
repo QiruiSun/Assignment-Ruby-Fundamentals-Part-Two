78f0e0fbb59f504b8529d3c6ae0b0a69213d16ce
Qirui Sun <qiruisun@gmail.com>
2016-01-13 15:01:04 -0500

    Practice arrays with exercize6

--- exercise6.rb
+++ exercise6.rb
@@ -7,4 +7,20 @@ grocery_list.each {|item| puts "*#{item}"}
 def listing_grocery(n)
   (n).each {|stuff| puts "*#{stuff}"}
 end
-  
+
+grocery_list.size
+
+if grocery_list.include?("banana")
+  puts "You need to pick up bananas."
+else
+  puts "You don't need to pick up bananas toady."
+end
+
+grocery_list[1]
+
+grocery_list.sort! # Thingking how to reverse the alphabetically ordered list using .sort{|x, y| y <=> x }
+
+listing_grocery(grocery_list)
+
+grocery_list.pop
+puts grocery_list
